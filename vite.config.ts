7ea8a5973ffe30240c72b1a3d1a00d707c1aa5ab
KvSmIr <casokgl@gmail.com>
2025-10-16 18:30:15 +0000

    Troisieme commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "https://github.com/KvSmIr/keglo-folio-chic/", // 👈 très important pour GitHub Pages !
+  base: "/keglo-folio-chic/", // 👈 très important pour GitHub Pages !
   server: {
     host: "::",
     port: 8080,
